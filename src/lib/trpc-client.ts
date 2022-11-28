2b5f5b7073bb9370401ff0d3f918902e19406af5
Luke Shafer <lukealanshafer@gmail.com>
2022-11-27 20:59:24 -0500

    img fixes

--- src/lib/trpc-client.ts
+++ src/lib/trpc-client.ts
@@ -1,11 +1,11 @@
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import { createTRPCProxyClient, httpBatchLink, httpLink } from '@trpc/client';
 import superjson from 'superjson';
 import type { AppRouter } from '../trpc/router/_index';
 
 export const client = createTRPCProxyClient<AppRouter>({
 	transformer: superjson,
 	links: [
-		httpBatchLink({
+		httpLink({
 			url: '/api/trpc',
 		}),
 	],
